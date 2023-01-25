b4aece42438e395ffb420d03bd09c532aa735d90
Kirill Kirsanov <kkirsanov@webim-team.ru>
2023-01-25 16:22:56 +0300

    config fix

--- mkdocs_png_to_webp/plugin.py
+++ mkdocs_png_to_webp/plugin.py
@@ -5,10 +5,13 @@ from PIL import Image
 class ConvertPngToWebpPlugin(BasePlugin):
     def on_files(self, files, config):
         for file in files:
-            if file.src_uri.endswith('.png'):
-                image = Image.open(file.src_uri)
-                file.dest_uri = file.dest_uri[:len(file.dest_uri) - 4] + ".webp"
-                image.save(file.dest_uri, format="webp")
+            if file.abs_src_path.endswith('.png'):
+                print(file.abs_src_path)
+                image = Image.open(file.abs_src_path)
+                file.abs_dest_path = file.abs_dest_path[:len(file.abs_dest_path) - 4] + ".webp"
+                image.save(file.abs_dest_path, format="webp")
+        return files
 
     def on_page_content(self, html, page, config, files):
         html = html.replace(".png", ".webp")
+        return html
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='mypackage',
+    name='mkdocs-png-to-webp',
     version='0.0.1',
     install_requires=[
         'mkdocs',
