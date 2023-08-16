b9ca691e0cc05218388586eaa0075256b7a1a8f3
mur4d1n <mur4d1n@yandex.ru>
2023-08-16 16:42:01 +0300

    config fix

--- setup.py
+++ setup.py
@@ -13,10 +13,10 @@ setup(
     url='https://github.com/mur4d1n',
     download_url='https://github.com/mur4d1n-lib/mkdocs-images-to-webp/archive/refs/tags/v_07.tar.gz',
     keywords=['mkdocs', 'images', 'webp'],
-)
 
-entry_points={
-    'mkdocs.plugins': [
-        'images-to-webp = mkdocs_images_to_webp.plugin:ConvertImagesToWebpPlugin',
-    ]
-}
+    entry_points={
+        'mkdocs.plugins': [
+            'images-to-webp = mkdocs_images_to_webp.plugin:ConvertImagesToWebpPlugin',
+        ]
+    }
+)
